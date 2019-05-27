ecc62031ba7e59cb07831d530f62435b8d39da15
rober19 <texas_telecaster@hotmail.com>
2019-05-27 09:48:02 -0500

    [rober19] [05_27_19] Fix connection

--- src/Datos/Conexion.java
+++ src/Datos/Conexion.java
@@ -21,11 +21,6 @@ public class Conexion {
 //    static String user="root";
 //    static String pass="Brayan97.";
 //    static String url="jdbc:mysql://127.0.0.1/basededatos_ferreteria";
-    static String url = "jdbc:mysql://arfo8ynm6olw6vpn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com/sez90fdieovgswva";
-    //        + "?autoReconnect=true&useSSL=false&useJDBCCompliantTimezoneShift=true&"
-    //        + "useLegacyDatetimeCode=false&serverTimezone=UTC";
-    static String user = "q3pvzlxp30tali5h";
-    static String pass = "jgd2tndc3pqet14v";
 
     public static Connection enlace() {
         if (cnn != null) {
@@ -90,7 +85,7 @@ public class Conexion {
     public static ResultSet enlaceDetalleComprobante(ResultSet rs) {
         try {
             sta = declaracion(sta);
-            rs = sta.executeQuery("select * from detallecomprobantes");
+            rs = sta.executeQuery("select * from DETALLECOMPROBANTES");
 
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error ");
@@ -111,13 +106,13 @@ public class Conexion {
 
     public static ResultSet idComprobante(ResultSet rs) throws SQLException {
         sta = declaracion(sta);
-        rs = sta.executeQuery("select idComprobante from Comprobantes");
+        rs = sta.executeQuery("select idComprobante from COMPROBANTES");
         return rs;
     }
 
     public static ResultSet enlaceComprobante(ResultSet rs) throws SQLException {
         sta = declaracion(sta);
-        rs = sta.executeQuery("select * from Comprobantes");
+        rs = sta.executeQuery("select * from COMPROBANTES");
         return rs;
     }
 
